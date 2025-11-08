b8ef8f77c559e070eecea2b17a554f363df147b6
vikram(wsl) <vikramjit360@gmail.com>
2025-11-08 15:50:49 +0530

    feat: add database response variants of types in types.ts

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,19 +1,50 @@
 import { z } from "zod";
 
+// Base response
+// This type is for craeting database response variants of the below types
+const baseResponseSchema = z.object({
+  id: z.string(),
+  createdAt: z.coerce.date(),
+  updatedAt: z.coerce.date(),
+});
+export type BaseResponse = z.infer<typeof baseResponseSchema>;
+
+// User
 export const userZodSchema = z.object({
-  username: z.string().min(3).max(50),
+  username: z.string(),
   password: z.string().min(8),
-  referralCode: z.string().length(8),
+  referralCode: z.string(),
   credits: z.number().default(0),
   referrerId: z.string().optional(),
 });
-
 export type User = z.infer<typeof userZodSchema>;
 
+// UserResponse
+export const userResponseZodSchema = userZodSchema.safeExtend(baseResponseSchema);
+export type UserResponse = z.infer<typeof userResponseZodSchema>;
+
+// Referral
 export const referralZodSchema = z.object({
   referrerId: z.string(),
   referredUserId: z.string(),
   status: z.enum(["PENDING", "CONVERTED"]).default("PENDING"),
 });
-
 export type ReferralInput = z.infer<typeof referralZodSchema>;
+
+// ReferralResponse
+export const referralResponseZodSchema = referralZodSchema.safeExtend(baseResponseSchema);
+export type ReferralResponse = z.infer<typeof referralResponseZodSchema>;
+
+// Book
+export const bookZodSchema = z.object({
+  title: z.string().min(1).max(200),
+  image: z.string(),
+  price: z.number().positive(),
+});
+export type BookInput = z.infer<typeof bookZodSchema>;
+
+// BookResponse
+export const bookResponseZodSchema = bookZodSchema.safeExtend(baseResponseSchema);
+export type BookResponse = z.infer<typeof bookResponseZodSchema>;
+
+
